1f66f80460e8cdd17cb4b7cc03b4f9712bb1a6d0
DESKTOP-QF059EM\user <fromhelianthus@gmail.com>
2024-11-06 15:32:49 +0900

    feat: remove category and move header styles to global.css

--- app/components/Header.js
+++ app/components/Header.js
@@ -8,70 +8,26 @@ import "../globals.css";
 export default function Header({ categories }) {
     return (
         <header>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "flex-start",
-                    marginLeft: "1%",
-                }}
-            >
-                <Link
-                    href="/"
-                    style={{
-                        fontSize: "20px",
-                        fontStyle: "italic",
-                        color: "#ddd",
-                        textDecoration: "none",
-                    }}
-                >
+            <div className="header-links">
+                <Link href="/" className="site-link">
                     junefromjuly
                 </Link>
-                <Link
-                    href="/about"
-                    style={{
-                        fontSize: "16px",
-                        fontStyle: "italic",
-                        color: "#fff",
-                        textDecoration: "none",
-                        marginTop: "4px",
-                    }}
-                >
+                <Link href="/about" className="about-link">
                     About
                 </Link>
             </div>
             <nav>
-                <ul
-                    style={{
-                        listStyleType: "none",
-                        margin: "0 20px",
-                        padding: "0",
-                        display: "flex",
-                        gap: "10px",
-                        overflowX: "auto", // 스크롤바가 필요할 때만 나타나도록 설정
-                        whiteSpace: "nowrap",
-                        scrollSnapType: "x mandatory",
-                    }}
-                >
-                    <li style={{ scrollSnapAlign: "start" }}>
-                        <Link
-                            href="/"
-                            style={{ color: "#fff", textDecoration: "none" }}
-                        >
+                <ul className="category-list">
+                    <li>
+                        <Link href="/" className="category-link">
                             All
                         </Link>
                     </li>
                     {categories.map((category) => (
-                        <li
-                            key={category.slug}
-                            style={{ scrollSnapAlign: "start" }}
-                        >
+                        <li key={category.slug}>
                             <Link
                                 href={`/categories/${category.slug}`}
-                                style={{
-                                    color: "#fff",
-                                    textDecoration: "none",
-                                }}
+                                className="category-link"
                             >
                                 {category.name}
                             </Link>
@@ -79,14 +35,6 @@ export default function Header({ categories }) {
                     ))}
                 </ul>
             </nav>
-            <style jsx>{`
-                @media (min-width: 768px) {
-                    nav ul {
-                        overflow-x: visible; // PC 화면에서는 스크롤 제거
-                        justify-content: center; // 넓은 화면에서 가운데 정렬
-                    }
-                }
-            `}</style>
         </header>
     );
 }

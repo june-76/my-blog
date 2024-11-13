493d4618d11dad912a3a542b48ee411f6f529f1c
June <fromhelianthus@gmail.com>
2024-11-13 23:57:20 +0900

    chore: add comments

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,11 +1,15 @@
 // app/components/Header.js
 
+// components 폴더는 그 자체로 라우팅되지 않으며, 재사용 가능한 컴포넌트들이 위치하는 곳입니다.
+
+// 해당 컴포넌트는 CSR임을 명시합니다.
 "use client";
 
 import Link from "next/link";
 import "../globals.css";
 
-export default function Header({ categories }) {
+// categories props를 받습니다. (카테고리 정보를 담고 있는 배열)
+export default function Header({ categories = [] }) {
     return (
         <header>
             <div className="header-links">
@@ -23,16 +27,24 @@ export default function Header({ categories }) {
                             All
                         </Link>
                     </li>
-                    {categories.map((category) => (
-                        <li key={category.slug}>
-                            <Link
-                                href={`/categories/${category.slug}`}
-                                className="category-link"
-                            >
-                                {category.name}
-                            </Link>
+                    {categories.length === 0 ? (
+                        <li>
+                            <span className="category-link">
+                                No categories available
+                            </span>
                         </li>
-                    ))}
+                    ) : (
+                        categories.map((category) => (
+                            <li key={category.slug}>
+                                <Link
+                                    href={`/categories/${category.slug}`}
+                                    className="category-link"
+                                >
+                                    {category.name}
+                                </Link>
+                            </li>
+                        ))
+                    )}
                 </ul>
             </nav>
         </header>

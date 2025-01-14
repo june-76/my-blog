bc5d59f28b242a475b072d8fc6d0812340828885
June <fromhelianthus@gmail.com>
2025-01-15 05:16:36 +0900

    feat: create EC2 API test

---
+++ app/api/posts/route.ts
@@ -0,0 +1,23 @@
+// app/api/posts/route.ts
+
+export async function GET(req: Request) {
+    try {
+        const apiUrl = "http://52.79.251.88:3000/api/posts";
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        return new Response(JSON.stringify(data), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error("API 호출 중 에러 발생:", error);
+        return new Response(
+            JSON.stringify({ error: "API 호출 중 오류가 발생했습니다." }),
+            {
+                status: 500,
+                headers: { "Content-Type": "application/json" },
+            }
+        );
+    }
+}
